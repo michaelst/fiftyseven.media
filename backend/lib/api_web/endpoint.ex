750e335b69f154c560abeb1761784627bbdfa079
Michael St Clair <michaelst57@gmail.com>
2020-08-17 18:49:48 -0600

    work on deploy

--- backend/lib/api_web/endpoint.ex
+++ backend/lib/api_web/endpoint.ex
@@ -2,16 +2,7 @@ defmodule FSMWeb.Endpoint do
   use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :fiftysevenmedia
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug(Plug.Static,
-    at: "/",
-    from: :fiftysevenmedia,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-  )
+  plug(CORSPlug)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -22,7 +13,7 @@ defmodule FSMWeb.Endpoint do
   end
 
   plug(Plug.RequestId)
-  plug(Plug.Logger)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
   plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
@@ -34,32 +25,5 @@ defmodule FSMWeb.Endpoint do
 
   plug(Plug.MethodOverride)
   plug(Plug.Head)
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug(Plug.Session,
-    store: :cookie,
-    key: "_api_key",
-    signing_salt: "+hz/1r0t"
-  )
-
-  plug(CORSPlug)
-
   plug(FSMWeb.Router)
-
-  @doc """
-  Callback invoked for dynamically configuring the endpoint.
-
-  It receives the endpoint configuration and checks if
-  configuration should be loaded from the system environment.
-  """
-  def init(_key, config) do
-    if config[:load_from_system_env] do
-      port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
-    else
-      {:ok, config}
-    end
-  end
 end
